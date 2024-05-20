55c6a1824b5e6577cd5134eeef2c879bdd142dc0
Noé Desprez <noe84starlight@gmail.com>
2024-05-20 17:02:43 +0200

    structure

--- Channel.hpp
+++ Channel.hpp
@@ -1,13 +1,26 @@
 #pragma once
 
+# include <string>
+# include <vector>
+# include <algorithm>
 
-
-//TO DO will be stored into a map in server instance
 class Channel
 {
-private:
-	/* data */
-public:
-	Channel(/* args */);
-	~Channel();
+private :
+	std::string	name;
+	std::string	pw; // empty means no password
+	std::string	topic; // ?
+	u_int16_t	max_users; // 0 means no limit
+	bool		_op_topic; // ?
+	bool		invite;
+
+	std::vector<std::string>	_users;
+	std::vector<std::string>	_operators;
+
+	void	remove_normal_user(Client &user);
+	void	remove_operator(Client &user);
+
+public :
+	Channel(const std::string &name);
+
 };
