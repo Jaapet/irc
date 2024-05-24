c3fc1e5e22fa04e4669fbc3faa4e228eb9338860
ndesprez <ndesprez@student.42nice.fr>
2024-05-24 18:49:17 +0200

    a

--- Channel.cpp
+++ Channel.cpp
@@ -36,3 +36,57 @@ void	Channel::set_invite(const bool &invite)
 {
 	this->invite = invite;
 }
+
+//-----------------------------------------------------------------------------------------------
+
+bool	Channel::is_op(std::string const &nickname)
+{
+	for (int i = 0; i < this->operators.size(); i++)
+	{
+		if (this->operators[i] == nickname)
+			return (true);
+	}
+	return (false);
+}
+
+
+//-----------------------------------------------------------------------------------------------
+
+bool	Channel::add_user(std::string const &nickname)
+{
+	if (this->invite && *std::find(this->invited_users.begin(), this->invited_users.end(), nickname) != nickname)
+		return (1);
+	if (*std::find(this->users.begin(), this->users.end(), nickname) == nickname)
+		return (2);
+	if (this->users.size() >= this->max_users)
+		return (3);
+	this->users.push_back(nickname);
+	return (0);
+}
+
+bool	Channel::rm_user(std::string const &nickname)
+{
+	std::string temp;
+
+	for (int i = 0; i < this->users.size(); i++)
+	{
+		if (this->users[i] == nickname)
+		{
+			this->users[i] = this->users[get_nmemb() - 1];
+			this->users[get_nmemb() - 1] = nickname;
+			this->users.pop_back();
+			return (true);
+		}
+	}
+	return (false);
+}
+
+void	Channel::invite_user(std::string const &nickname)
+{
+	for (int i = 0; i < this->invited_users.size(); i++)
+	{
+		if (this->invited_users[i] == nickname)
+			return ;
+	}
+	this->invited_users.push_back(nickname);
+}
