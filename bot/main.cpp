987c5c04e18033a6494888957993e6cb979a1803
Edilson Firmino <edfirmin@c2r3p5.42nice.fr>
2024-05-31 18:49:15 +0200

    bot ok

--- bot/main.cpp
+++ bot/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edfirmin <edfirmin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 14:03:15 by edfirmin          #+#    #+#             */
-/*   Updated: 2024/05/31 17:50:58 by edfirmin         ###   ########.fr       */
+/*   Updated: 2024/05/31 18:47:20 by edfirmin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,58 @@ int check_log(char *buff)
     if (line != "" && line.compare("001"))
         return (0);
     return (1);
+}
+
+std::vector<std::string>  splitBuffer(std::string const &str)
+{
+    std::string del = "\r\n";
+    std::string tmp2;
+    std::string tmp = str;
+    size_t  f = 4;
+    std::vector<std::string> com;
+
+    while (f != std::string::npos)
+    {
+        f = tmp.find(del);
+        tmp2 = tmp.substr(0, f);
+		if (!tmp2.empty())
+        	com.push_back(tmp2);
+        if (f == std::string::npos)
+            break;
+        tmp = tmp.substr(f + del.size(), tmp.size() - 1);
+    }
+    return (com);
+}
+
+std::vector<std::string> split(const std::string& str, char delimiter) 
+{
+    std::vector<std::string> tokens;
+    std::stringstream ss(str);
+    std::string token;
+
+    while (std::getline(ss, token, delimiter)) 
+	{
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
+void send_mp(char *buff, int bot_sock)
+{
+    std::vector<std::string>   tab = splitBuffer(buff);
+    std::vector<std::string>   ttab;
+    std::string pre = "PRIVMSG ";
+    std::string suf = " :Hi mate, welcome to our IRC server, hope you enjoy it :)\r\n";
+    std::string com;
+    for (size_t i = 0; i < tab.size() - 2; i++)
+    {
+        ttab = split(tab[i], ' ');
+        if (ttab[1].compare("352"))
+            break;
+        com += pre + ttab[7] + suf;
+    }
+    send(bot_sock, com.c_str(), std::strlen(com.c_str()), 0);
 }
 
 int main(int argc, char **argv)
@@ -68,7 +120,7 @@ int main(int argc, char **argv)
             shutdown(bot_sock, SHUT_RDWR);
             throw (BotCreationFail());//modif
         }
-        std::string mes = "PASS " + pass + "\r\nNICK " + bot_nick + "\r\nUSER bot 0 * :Robot-Boy";
+        std::string mes = "PASS " + pass + "\r\nNICK " + bot_nick + "\r\nUSER bot 0 * :Robot-Boy\r\n";
         send(bot_sock, mes.c_str(), std::strlen(mes.c_str()), 0);
         size_t i = recv(bot_sock, buff, sizeof(buff) - 1, 0);
         buff[i - 1] = '\0';
@@ -78,18 +130,17 @@ int main(int argc, char **argv)
             throw (BotCreationFail());//modif
         }
 
-        mes = "WHO 0";
+        mes = "WHO 0\r\n";
         send(bot_sock, mes.c_str(), std::strlen(mes.c_str()), 0);
         size_t j = recv(bot_sock, buff, sizeof(buff) - 1, 0);
         buff[j - 1] = '\0';
         
-        std::cout << buff << std::endl;
-        mes = "QUIT";
+        send_mp(buff, bot_sock);
+
+        mes = "QUIT\r\n";
         send(bot_sock, mes.c_str(), std::strlen(mes.c_str()), 0);
         size_t k = recv(bot_sock, buff, sizeof(buff) - 1, 0);
         buff[k - 1] = '\0';
-        
-        shutdown(bot_sock, SHUT_RDWR);
     }
     catch(const std::exception& e)
     {
